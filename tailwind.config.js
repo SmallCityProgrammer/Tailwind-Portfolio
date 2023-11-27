3d907a7c85ef41dee20638245487b02599d71115
Eliezer Castro <smallcity.programmer@gmail.com>
2023-11-27 10:39:50 -0300

    deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}","./node_modules/flowbite/*.js"],
+  content: [
+    "./dist/*.{html,js}",
+    "./node_modules/flowbite/*.js",
+    "node_modules/preline/dist/*.js"],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
-    require('flowbite/plugin')],
+    require('flowbite/plugin'),
+    require('preline/plugin')],
+    
 }
 
